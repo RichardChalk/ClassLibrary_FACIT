589baa07a1c1087e27bc8a14e4e3e841c877b612
Richard Chalk <barmybrit@gmail.com>
2022-11-30 14:53:53 +0100

    #13 Looped through all products in Northwind in Project3

--- Project3/Program.cs
+++ Project3/Program.cs
@@ -1,4 +1,18 @@
 ﻿// Först ska vi installerar alla Nuget paket som behövs för att kunna
 // koppla upp oss mot en databas...
 
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using SuperDuperClassLibrary.Models;
+
 Console.WriteLine("Project 3 FTW");
+
+// Koppla till Northwind DbContext
+Console.WriteLine("************** Northwind ***************");
+var dbContext = new NorthwindContext();
+
+var counter = 1;
+foreach (var prod in dbContext.Products)
+{
+    Console.WriteLine($"{counter}: {prod.ProductName}");
+    ++counter;
+}
