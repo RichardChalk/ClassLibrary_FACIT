dbb246d560bb371c07b561d930ac2572111404bc
Richard Chalk <barmybrit@gmail.com>
2022-11-30 14:23:37 +0100

    #10 Added nuget packages for connectiong to db

--- Project3/Program.cs
+++ Project3/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Först ska vi installerar alla Nuget paket som behövs för att kunna
+// koppla upp oss mot en databas...
+
+
