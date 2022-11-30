31cae9dac5645721d6c953548253641c12142269
Richard Chalk <barmybrit@gmail.com>
2022-11-30 12:53:19 +0100

    #5 Skapa fyra metoder som returner resultatet av en matematisk beräkning

--- SuperDuperClassLibrary/MathServices.cs
+++ SuperDuperClassLibrary/MathServices.cs
@@ -2,6 +2,21 @@
 {
     public class MathServices
     {
-
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public double Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
     }
 }
