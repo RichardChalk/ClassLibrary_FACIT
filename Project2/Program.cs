a68ad6e4cc0104987ca34ccf21cf3c3d1ced48c6
Richard Chalk <barmybrit@gmail.com>
2022-11-30 13:22:41 +0100

    #8 Skapade Project2

---
+++ Project2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Hej
+// Jag heter project 2!
+// Jag har hört att det finns ett bibliotek med fantastiska matematiska metoder!
+// Jag vill gärna få tillgång till dem!
+
+using SuperDuperClassLibrary;
+
+var service = new MathServices();
+var resultToConsole = service.Multiplication(5, 10);
+Console.WriteLine($"Jag anropade en metod som heter Multiplication() i class library");
+Console.WriteLine($"Jag skickade med 2 parametrar (talen 5 & 10)");
+Console.WriteLine($"Reslutatet blev: {resultToConsole}");
+
+Console.WriteLine("================================================================");
+
+var resultToConsoleDouble = service.Division(5, 10);
+Console.WriteLine($"Jag anropade en metod som heter Division() i class library");
+Console.WriteLine($"Jag skickade med 2 parametrar (talen 5 & 10)");
+Console.WriteLine($"Reslutatet blev: {resultToConsoleDouble}");
+
+
--- SuperDuperClassLibrary/MathServices.cs
+++ SuperDuperClassLibrary/MathServices.cs
@@ -14,7 +14,7 @@
         {
             return num1 * num2;
         }
-        public double Division(int num1, int num2)
+        public double Division(double num1, double num2)
         {
             return num1 / num2;
         }
