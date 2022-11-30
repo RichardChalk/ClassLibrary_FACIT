7252ead1ba21dc00af28abeeaa0c552ed93a0f34
Richard Chalk <barmybrit@gmail.com>
2022-11-30 13:13:41 +0100

    #7 Vi anropade Class Library från Project1

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Hej
+// Jag heter project 1!
+// Jag har hört att det finns ett bibliotek med fantastiska matematiska metoder!
+// Jag vill gärna få tillgång till dem!
+
+using SuperDuperClassLibrary;
+
+var service = new MathServices();
+var resultToConsole = service.Addition(5,10);
+Console.WriteLine($"Jag anropade en metod som heter Addition() i class library");
+Console.WriteLine($"Jag skickade med 2 parametrar (talen 5 & 10)");
+Console.WriteLine($"Reslutatet blev: {resultToConsole}");
+
+Console.WriteLine("================================================================");
+
+resultToConsole = service.Subtraction(5, 10);
+Console.WriteLine($"Jag anropade en metod som heter Subtraction() i class library");
+Console.WriteLine($"Jag skickade med 2 parametrar (talen 5 & 10)");
+Console.WriteLine($"Reslutatet blev: {resultToConsole}");
+
